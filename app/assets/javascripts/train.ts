7af2f89fc81475645dca951eb9acef2c425c5802
Vladimir Okhotnikov <vokhotnikov@gmail.com>
2017-01-06 10:25:27 +0300

    Focus back in the field after sound play

--- app/assets/javascripts/train.ts
+++ app/assets/javascripts/train.ts
@@ -1,22 +1,21 @@
-/**
- * Created by hunter on 15/12/2016.
- */
-
 function checkAnswer(e : Event) {
     if (e.preventDefault) e.preventDefault();
 
-    $(".incorrect").hide();
-    $(".correct").hide();
+    const incorrect = $(".incorrect");
+    const correct = $(".correct");
 
-    var answer = $("#answer").val();
+    incorrect.hide();
+    correct.hide();
+
+    const answer = $("#answer").val();
     if ($.inArray(answer.trim().toLowerCase(), (<any>window).candidates) > -1) {
-        $(".correct").show();
+        correct.show();
         setTimeout(() => window.location.reload(false), 1500);
     } else {
-        $(".incorrect").show();
+        incorrect.show();
         setTimeout(() => {
             $("#answer").val("").focus();
-            $(".incorrect").hide();
+            incorrect.hide();
         }, 2000);
     }
 
@@ -29,6 +28,7 @@ $(() => {
     if ($("audio").length) {
         $("h2").click(() => {
             ($("audio")[0] as HTMLAudioElement).play();
+            $("#answer").focus();
         });
     }
 });
